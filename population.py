313fbf8f03d6cba65bd2380c6df998ec6c1321ab
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-27 18:38:01 +0100

    Add files via upload

---
+++ population.py
@@ -0,0 +1,17 @@
+import requests
+import streamlit as st
+
+base='light'
+backgroundColor ='white'
+st.write("""
+
+# Simple Population Web App 
+
+""")
+col1, col2 = st.columns(2)
+
+# Display the images in the columns
+col1.image('worldimage.jpg')
+col2.image('flags.jpg')
+
+
