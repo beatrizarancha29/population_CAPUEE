15987aef2405464b82fe2dc409e8a435c32259f9
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-28 18:56:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,15 @@ backgroundColor ='white'
 st.write("""
 # Simple Population Web App 
 """)
-col1, col2 = st.columns(2)
+col1, col2, col3, = st.columns(3)
 # Display the images in the columns
 col1.image('worldimage.jpg')
 col2.image('flags.jpg')
+col3.image(
+
+
+
+
 st.write( """
 ## Countries with the largest population
 """)
