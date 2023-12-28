c643abcf29087bde43cdec8d0cd6bd727886c061
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-28 12:54:58 +0100

    Update api_connection.py

--- api_connection.py
+++ api_connection.py
@@ -7,16 +7,15 @@ import numpy as np
 
 # create GET request
 
-def get_data_from_api():
+def get_us_population():
     
     url = "https://world-geo-data.p.rapidapi.com/countries/US"
     header = {
 	"X-RapidAPI-Key": "e8dcb32d36msh2417d6829510541p15ff3ajsnf7e49772d415",
 	"X-RapidAPI-Host": "world-geo-data.p.rapidapi.com"
 }
-    params = 
 
-    response = requests.get(url, headers=headers, params=params)
-    outputs = response.json()
-
-    return outputs
+response = requests.get(url, headers=headers, params=params)
+outputs = response.json()
+population = outputs['population']
+return population
