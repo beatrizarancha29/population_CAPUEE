6ce9b76436280dadeca50cf8190832b391ec2ee5
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-28 16:15:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,20 @@ st.write( """
 
 col1, col2, col3 = st.columns(3)
 
-population = get_population_data("United States", "US")
+population_us = get_population_data("United States", "US")
 col1.image('usa.png')
 col1.write("USA")
-col1.write(f" Population: {population}")
+col1.write(f" Population: {population_us}")
 
-population = get_population_data("India", "IN")
+population_in = get_population_data("India", "IN")
 col2.image('india')
 col2.write("India")
-col2.write(f" Population: {population}")
+col2.write(f" Population: {population_in}")
 
-population = get_population_data("China", "CN")
+population_cn = get_population_data("China", "CN")
 col2.image('china.png')
 col2.write("China")
-col2.write(f" Population: {population}")
+col2.write(f" Population: {population_cn}")
 
 # Display the text in each column
 #col1.write("USA")
