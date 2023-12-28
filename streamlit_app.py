280b66dfdfaadd40481134d5c3e32ef156426b22
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-28 13:22:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,16 @@ col3.write("India")
 ##############################################################################
 
 def get_us_population():
-    
     url = "https://world-geo-data.p.rapidapi.com/countries/US"
-    header = {
-	"X-RapidAPI-Key": "e8dcb32d36msh2417d6829510541p15ff3ajsnf7e49772d415",
-	"X-RapidAPI-Host": "world-geo-data.p.rapidapi.com"
-		}
+    headers = {
+        "X-RapidAPI-Key": "e8dcb32d36msh2417d6829510541p15ff3ajsnf7e49772d415",
+        "X-RapidAPI-Host": "world-geo-data.p.rapidapi.com"
+    }
 
-	response = requests.get(url, headers=headers, params=params)
-	outputs = response.json()
-	population = outputs['population']
-	return population
+    response = requests.get(url, headers=headers)
+    outputs = response.json()
+    population = outputs['population']
+    return population
 
 population = get_us_population()
 st.write(f"Population USA: {population}")
