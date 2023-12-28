ae3b984f39864778c737d848de84a7e19e5aaf96
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-28 13:19:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,12 @@ def get_us_population():
     header = {
 	"X-RapidAPI-Key": "e8dcb32d36msh2417d6829510541p15ff3ajsnf7e49772d415",
 	"X-RapidAPI-Host": "world-geo-data.p.rapidapi.com"
-}
+		}
 
-response = requests.get(url, headers=headers, params=params)
-outputs = response.json()
-population = outputs['population']
-return population
+	response = requests.get(url, headers=headers, params=params)
+	outputs = response.json()
+	population = outputs['population']
+	return population
 
 population = get_us_population()
 st.write(f"Population USA: {population}")
