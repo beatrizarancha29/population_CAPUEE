0a563e680d960b39ce19329b74cb3dd891a7b112
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-31 12:30:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 import pandas as pd 
 import numpy as np
-#import matplotlib.pyplot
+import matplotlib.pyplot
 from api_connection import get_population_data
 from api_connection import country_data
 
