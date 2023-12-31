762435652ac90afd1cf2f7b8f9a00f94883ea70b
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2023-12-31 10:57:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ import numpy as np
 from api_connection import get_population_data
 from api_connection import country_data
 
-import matplotlib
-
 base='light'
 backgroundColor ='white'
 st.write("""
