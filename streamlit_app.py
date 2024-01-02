0acdb80f5a36d29367cc7cf57960520d946b2536
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2024-01-02 16:13:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot
 #import plotly
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 from api_connection import get_population_data
 from api_connection import country_data
 
