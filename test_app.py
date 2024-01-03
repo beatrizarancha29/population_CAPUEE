f0d215188ac45f89aeb5d3672a63cfa25fd279f0
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2024-01-03 17:52:44 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,8 +2,4 @@ import pandas as pd
 import plotly.express as px
 base='light'
 backgroundColor ='white'
-st.write("HELLO")
-data = {'Country': ['A', 'B', 'C'], 'Population': [10, 20, 30]}
-df = pd.DataFrame(data)
-
-fig = px.pie(df, values='Population', names='Country', title='Population Distribution by Country')
+st.write("####HELLO")
