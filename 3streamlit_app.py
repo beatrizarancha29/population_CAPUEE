3736404d6fa893186ab1b3fadd525d5598c12d6c
beatrizarancha29 <152521496+beatrizarancha29@users.noreply.github.com>
2024-01-04 20:09:58 +0100

    Update 3streamlit_app.py

--- 3streamlit_app.py
+++ 3streamlit_app.py
@@ -142,13 +142,14 @@ df = pd.DataFrame(data)
 
 # Display the Streamlit app
 st.write("# Population Density Bubble Chart")
-st.plotly_chart(fig)
+
 
 fig = go.Figure(data=[go.Scatter(
     x='Area', y='Population Density',size='Population Density', hover_name='Country',
                  labels={'Area': 'Area (Square Units)', 'Population Density': 'Population Density'},
                  title='Population Density Bubble Chart')])
 
+st.plotly_chart(fig)
 #####################################################################################################
 selected_country = st.selectbox('Select a Country', [country[0] for country in country_data])
 selected_iso_code = [country[1] for country in country_data if country[0] == selected_country][0]
